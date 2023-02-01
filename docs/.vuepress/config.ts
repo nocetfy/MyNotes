6de4f4126bb2bfaab8f26ea8d59d411ae2a64651
nocetfy <nocetfy@gmail.com>
2023-02-01 15:38:59 +0800

    feat: modify name

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -16,7 +16,7 @@ export default defineUserConfig({
   description: "just learn",
 
   // 网站路径默认为主域名。如果网站部署在子路径下，比如 xxx.com/yyy，那么 base 应该被设置为 "/yyy/"
-  base: "/myNotes",
+  base: "/MyNotes",
 
   theme,
   // 是否开启页面预拉取，如果服务器宽带足够，可改为 true，会提升其他页面加载速度
